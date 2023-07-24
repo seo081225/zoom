53b85c233430567ccbd4a5f60cd02d9547e12a49
seo <seo081225@gmail.com>
2023-07-24 19:45:44 +0900

    1.6 Chat Completed

--- src/server.js
+++ src/server.js
@@ -19,15 +19,14 @@ function onSocketClose() {
     console.log("Disconnected from the Browser ❌");
 }
 
-function onSocketMessage(message) {
-    console.log(message);
-}
-
+const sockets = [];
 wss.on("connection", (socket) => {
     console.log("Connected to Browser ✅");
+    sockets.push(socket);
     socket.on("close", onSocketClose);
-    socket.on("message", onSocketMessage);
-    socket.send("hello!!!");
+    socket.on("message", (message) => {
+        sockets.forEach((aSocket) => aSocket.send(message));
+    });
 });
 
 server.listen(3000, handleListen);
