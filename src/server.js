0edc2d000dfd8f4e9aaf93ffefcd0d1f77880244
seo <seo081225@gmail.com>
2023-07-24 19:57:03 +0900

    1.8 Nicknames part Two

--- src/server.js
+++ src/server.js
@@ -23,9 +23,18 @@ const sockets = [];
 wss.on("connection", (socket) => {
     console.log("Connected to Browser ✅");
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     socket.on("close", onSocketClose);
-    socket.on("message", (message) => {
-        sockets.forEach((aSocket) => aSocket.send(message));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
     });
 });
 
