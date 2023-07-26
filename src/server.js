38d836f59edf81427f7d7ac5cb8b086e5f0230a6
seo <seo081225@gmail.com>
2023-07-26 20:08:06 +0900

    2.2 SocketIO is Amazing

--- src/server.js
+++ src/server.js
@@ -14,7 +14,12 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-    console.log(socket);
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+            done();
+        }, 10000);
+    });
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
